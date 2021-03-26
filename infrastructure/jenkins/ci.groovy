b0bff4fa7e642bf038214204045c868b944ac564
Peter Moser <p.moser@noi.bz.it>
2021-03-26 17:16:33 +0100

    infra: Add new infrastructure folder

--- infrastructure/jenkins/ci.groovy
+++ infrastructure/jenkins/ci.groovy
@@ -1,11 +1,13 @@
 pipeline {
     agent {
         dockerfile {
-            filename 'docker/Dockerfile'
-            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
+            filename 'infrastructure/docker/node.dockerfile'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)'
         }
     }
-
+    options {
+        ansiColor('xterm')
+    }
     stages {
         stage('Dependencies') {
             steps {
