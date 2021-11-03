25b940e1285ef7d5fb113b3cb211034e72297851
Peter Moser <p.moser@noi.bz.it>
2021-11-03 17:12:18 +0100

    dev: remove unnecessary flag

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,7 +7,6 @@ const Dotenv = require('dotenv-webpack')
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  watch: true,
   output: {
     path: path.resolve(__dirname, './work/src/js/webcomponent'),
     filename: 'noi_maps_widget.min.js'
